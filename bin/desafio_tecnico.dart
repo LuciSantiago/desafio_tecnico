7223c2fe2e9bf5e9ddd49c3fb09322b2d06a42f6
LuciSantiago <lucianosantiago.dev@gmail.com>
2022-08-29 14:20:57 -0300

    update

--- bin/desafio_tecnico.dart
+++ bin/desafio_tecnico.dart
@@ -3,7 +3,19 @@ import 'dart:io';
 import 'package:desafio_tecnico/desafio_tecnico.dart';
 
 void main() {
-  int entrada = int.parse(stdin.readLineSync() ?? '0');
-  int resultado = somaDivisiveis(entrada);
-  print('Resultado: $resultado');
+  bool condicao = true;
+  int entrada = 0;
+  int resultado = 0;
+
+  while (condicao) {
+    print('Digite o número ou digite 0 para sair ');
+    entrada = int.parse(stdin.readLineSync() ?? '0');
+    if (entrada > 0) {
+      resultado = somaDivisiveis(entrada);
+      print('Resultado: $resultado');
+    } else {
+      print('Você saiu');
+      condicao = false;
+    }
+  }
 }
